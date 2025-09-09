085ef94d98c1467e826ab55c109c28980f1f9789
maycoday <202401080047@mitaoe.ac.in>
2025-09-09 18:52:03 +0530

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+@app.get("/plan/simple")
+def simple_plan():
+    df = pd.read_csv("../data/sample/daily_status_2025-09-09.csv")
+    plan = []
+    for _, r in df.iterrows():
+        if pd.isna(r['telecom_cert']) or r['telecom_cert'] == "":
+            plan.append({"train_id": r['train_id'], "status": "Maintenance"})
+        else:
+            plan.append({"train_id": r['train_id'], "status": "Ready"})
+    return {"plan": plan}
